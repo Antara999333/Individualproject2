a8f173ff84d48a2fdbc7e438bf9c14a7fea5eb7d
Antara999333 <143966099+Antara999333@users.noreply.github.com>
2023-10-02 19:58:36 -0400

    Update query.py

--- mylib/query.py
+++ mylib/query.py
@@ -2,15 +2,19 @@
 
 import sqlite3
 
+import sqlite3
 
-def query():
-    """Query the database for the top 5 rows of the GroceryDB table"""
+def query_bottom_5():
+    """Query the database for the bottom 5 rows of the GroceryDB table"""
     conn = sqlite3.connect("GroceryDB.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM GroceryDB")
-    print("Top 5 rows of the GroceryDB table:")
+    # Use ORDER BY to sort the rows in reverse order by the primary key (assuming there's a primary key column)
+    # and LIMIT to get the bottom 5 rows.
+    cursor.execute("SELECT * FROM GroceryDB ORDER BY primary_key_column DESC LIMIT 5")
+    print("Bottom 5 rows of the GroceryDB table:")
     print(cursor.fetchall())
     conn.close()
     return "Success"
+
 
 
